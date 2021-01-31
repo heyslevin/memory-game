f45b3b347b3ed113413b90f5627c4658c0f2ba77
memellaguno <memellaguno@gmail.com>
2021-01-31 13:50:40 -0600

    Building static version

--- src/App.js
+++ src/App.js
@@ -12,30 +12,48 @@ import soldado from "./img/soldado.jpg";
 import valiente from "./img/valiente.jpg";
 import venado from "./img/venado.jpg";
 
+function CurrentScore(props) {
+  return (
+    <div className="flex-small one-fourth">
+      <p className="no-margin-bottom">Current Score:</p>
+      <h2 className="no-top">1</h2>
+    </div>
+  );
+}
+
+function HighScore(props) {
+  return (
+    <div className="flex-small one-fourth">
+      <p className="no-margin-bottom">High Score</p>
+      <h2 className="no-top gray-text">0</h2>
+    </div>
+  );
+}
+
+function Header(props) {
+  return (
+    <div>
+      <div className="flex-row">
+        <div className="flex-small half">
+          <h1 className="no-margin-top">Memory Game</h1>
+          <p className="gray-text">
+            The cards get shuffled every time they are clicked. You CAN NOT
+            click on any card more than once or else your score resets to zero.{" "}
+          </p>
+        </div>
+        <HighScore />
+        <CurrentScore />
+      </div>
+      <hr />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <div className="small-container">
-        <div className="flex-row">
-          <div className="flex-small half">
-            <h1 className="no-margin-top">Memory Game</h1>
-            <p className="gray-text">
-              The cards get shuffled every time they are clicked. You CAN NOT
-              click on any card more than once or else your score resets to
-              zero.{" "}
-            </p>
-          </div>
-          <div className="flex-small one-fourth">
-            <p className="no-margin-bottom">High Score</p>
-            <h2 className="no-top gray-text">0</h2>
-          </div>
-          <div className="flex-small one-fourth">
-            <p className="no-margin-bottom">Current Score:</p>
-            <h2 className="no-top">1</h2>
-          </div>
-        </div>
-        <hr />
-
+        <Header />
         <div className="flex-row">
           <div className="flex-small one-fourth card red">
             <img src={valiente} alt="" className="responsive-image" />
