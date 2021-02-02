1cdde67f9f849ba2408e95b22cd0ee1a1e138e55
memellaguno <memellaguno@gmail.com>
2021-02-02 08:28:01 -0600

    All Components separated

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,21 +1,61 @@
 import React from "react";
 import Card from "./Card";
 
+import apache from "../img/apache.jpg";
+import barril from "../img/barril.jpg";
+import camaron from "../img/camaron.jpg";
+import corazon from "../img/corazon.jpg";
+import diablo from "../img/diablo.jpg";
+import jaras from "../img/jaras.jpg";
+import luna from "../img/luna.jpg";
+import melon from "../img/melon.jpg";
+import sirena from "../img/sirena.jpg";
+import soldado from "../img/soldado.jpg";
+import valiente from "../img/valiente.jpg";
+import venado from "../img/venado.jpg";
+
 function Board(props) {
+  let allCards = [
+    apache,
+    barril,
+    camaron,
+    corazon,
+    diablo,
+    jaras,
+    luna,
+    melon,
+    sirena,
+    soldado,
+    valiente,
+    venado,
+  ];
+
+  function cardShuffler() {
+    let cardArray = [];
+
+    for (let i = 0; i <= allCards.length; i++) {
+      cardArray.push(i);
+    }
+
+    console.log(cardArray);
+  }
+
+  cardShuffler();
+
   return (
     <div>
       <div className="flex-row">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card card={allCards[10]} />
+        <Card card={barril} />
+        <Card card={camaron} />
+        <Card card={corazon} />
       </div>
 
       <div className="flex-row">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card card={diablo} />
+        <Card card={jaras} />
+        <Card card={luna} />
+        <Card card={melon} />
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,10 @@
 import React from "react";
 
-import apache from "../img/apache.jpg";
-import barril from "../img/barril.jpg";
-import camaron from "../img/camaron.jpg";
-import corazon from "../img/corazon.jpg";
-import diablo from "../img/diablo.jpg";
-import jaras from "../img/jaras.jpg";
-import luna from "../img/luna.jpg";
-import melon from "../img/melon.jpg";
-import sirena from "../img/sirena.jpg";
-import soldado from "../img/soldado.jpg";
-import valiente from "../img/valiente.jpg";
-import venado from "../img/venado.jpg";
-
 function Card(props) {
+  console.log(props);
   return (
     <div className="flex-small one-fourth card red">
-      <img src={valiente} alt="" className="responsive-image" />
+      <img src={props.card} alt={props.card} className="responsive-image" />
     </div>
   );
 }
